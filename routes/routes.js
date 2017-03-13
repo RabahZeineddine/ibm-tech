6955de27bd948c7f9bcc7c3e075159f11af2b4fb
Rabah Zeineddine <Rabah@Rabahs-MacBook-Pro.local>
2017-03-13 15:08:36 -0300

    Added new feautures

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,23 @@
 module.exports = {
-	index : function(req, res) {
-		res.render('index.html');
-	},
-//	listRooms : function(req, res) {
-		 
-//		res.render('listRooms.html');
-//	},
-	accessDenied: function(req,res){
+    index: function (req, res) {
+        var token = req.session.user_token;
+        if (token !== null && token != '') {
+            res.render('index.html', {
+                authenticated: token
+            });
+        }
+        else {
+            res.render('index.html');
+        }
+    }
+    , //	listRooms : function(req, res) {
+    //		res.render('listRooms.html');
+    //	},
+    accessDenied: function (req, res) {
         var error = req.session.error;
         req.session.error = '';
-		res.render('index.html',{error: error});
-	}
+        res.render('index.html', {
+            error: error
+        });
+    }
 }
